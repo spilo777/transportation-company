f564422349b3bc0ccc7258e18f3f482d98389c6b
spilo <kapagiridis@gmail.com>
2023-04-20 09:05:03 +0300

    !!!

---
+++ scrip.js
@@ -0,0 +1,20 @@
+
+
+
+document.querySelector('button').addEventListener('click', function () {
+
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    EventBus.subscribe('send-feedback:insales:ui_feedback', function (data) {
+        ym(61230613, 'reachGoal', 'comparison'); return true;
+    });
+    EventBus.logger.add('quick_checkout')
+}, { 'once': 'true' });
+
+
+
+
